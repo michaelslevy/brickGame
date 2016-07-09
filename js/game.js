51808591c24ec43763743c05001deeb8c35fd169
MichaelsLevy <michael.levy.vt@gmail.com>
2016-07-09 13:49:29 -0400

    FEAT: added offset to max paddle position

--- js/game.js
+++ js/game.js
@@ -11,12 +11,23 @@ document.getElementById("game").addEventListener("mousemove", movePaddle);
 
 function movePaddle(e){   
     
+    //get paddle width
+    var paddleWidth=document.getElementById("paddle").getAttribute('width');
+    
     //get offset
     var rect = document.getElementById("boundary").getBoundingClientRect();
     var offset=rect.left;
+    var max=rect.right-paddleWidth-offset;
+    
+    
     
     //get mouse X coordinate
-    var mouseX =e.clientX-offset;
-
-   document.getElementById("paddle").setAttribute("x",mouseX);
+    var mouseX =e.clientX;
+    var newPosition = mouseX-offset;
+    
+    console.log(max, newPosition);
+    
+    if(newPosition<max){
+        document.getElementById("paddle").setAttribute("x",newPosition);
+    }
 }    
